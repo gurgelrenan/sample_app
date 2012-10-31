b73e24d444272b9e46bae539047c3f6fb827fa8b
Renan Gurgel <gurgel.renan@gmail.com>
2012-10-31 18:04:43 -0300

    login pela metade

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,11 @@
 module SessionsHelper
+	def sign_in(user)
+		#.permanent = expires: 20.years.from_now.utc 
+		cookies.permanent[:remember_token] = user.remember_token
+		self.current_user = user
+	end
+
+	def current_user=(user)
+		@current_user = user
+	end
 end
